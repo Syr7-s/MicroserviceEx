f0b2b78de2330b4b2dca371f7118b55077ffd569
Syr7-s <isasayar7@github.com>
2021-11-13 22:07:05 +0300

    StudentRepository interface was created.

---
+++ student-service/src/main/java/com/syrisa/studentservice/repository/StudentRepository.java
@@ -0,0 +1,10 @@
+package com.syrisa.studentservice.repository;
+
+import com.syrisa.studentservice.entity.Student;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends MongoRepository<Student,Long> {
+    
+}
