86015e7ebc83df5906374894f443cceb8fb1316c
Syr7-s <isasayar7@github.com>
2021-11-13 20:50:50 +0300

    HomeController class was created and openapi-ui was added pom.xml file

---
+++ web-app-bff/src/main/java/com/syrisa/webappbff/controller/HomeController.java
@@ -0,0 +1,17 @@
+package com.syrisa.webappbff.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.function.Supplier;
+
+@RestController
+@RequestMapping("/api/v1")
+public class HomeController {
+    @GetMapping("/home")
+    public String welcome(){
+        return "Welcome";
+    }
+
+}
