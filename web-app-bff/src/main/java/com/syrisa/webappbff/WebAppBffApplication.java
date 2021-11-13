3dadc3887024160237df225626f9a46371a4c08d
Syr7-s <isasayar7@github.com>
2021-11-13 20:36:11 +0300

    initial commit for web-app-bff

---
+++ web-app-bff/src/main/java/com/syrisa/webappbff/WebAppBffApplication.java
@@ -0,0 +1,13 @@
+package com.syrisa.webappbff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAppBffApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebAppBffApplication.class, args);
+    }
+
+}
---
+++ web-app-bff/src/test/java/com/syrisa/webappbff/WebAppBffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.syrisa.webappbff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebAppBffApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
