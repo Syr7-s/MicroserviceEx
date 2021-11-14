3dd9f8230149b7061b7df66eb57d3ad1389f4a29
Syr7-s <isasayar7@github.com>
2021-11-14 16:53:13 +0300

    initial commit for generate-service.

---
+++ generate-service/src/main/java/com/syrisa/generateservice/GenerateServiceApplication.java
@@ -0,0 +1,13 @@
+package com.syrisa.generateservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenerateServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GenerateServiceApplication.class, args);
+    }
+
+}
