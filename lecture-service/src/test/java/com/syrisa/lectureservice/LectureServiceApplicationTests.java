fa5e2a0b284cb86c4ea89d7d38916523f72c6de6
Syr7-s <isasayar7@github.com>
2021-11-14 15:55:05 +0300

    initial commit for lecture-service

---
+++ lecture-service/src/test/java/com/syrisa/lectureservice/LectureServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.syrisa.lectureservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LectureServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
