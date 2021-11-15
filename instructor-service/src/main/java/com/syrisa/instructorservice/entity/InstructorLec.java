6ca4d4225dfa683a163917aab1c264fe411c0732
Syr7-s <isasayar7@github.com>
2021-11-15 22:58:28 +0300

    InstructorLec was modified.

--- instructor-service/src/main/java/com/syrisa/instructorservice/entity/InstructorLec.java
+++ instructor-service/src/main/java/com/syrisa/instructorservice/entity/InstructorLec.java
@@ -1,4 +1,22 @@
 package com.syrisa.instructorservice.entity;
 
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDate;
+
+@Data
+@Builder
+@Document
 public class InstructorLec {
+    @Id
+    private Long instructorLecID;
+    private String lectureCode;
+    private String instructorID;
+    private String instructorNameSurname;
+    private int studentCapacity;
+    private LocalDate openDate;
+
 }
