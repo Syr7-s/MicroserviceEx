7dbea9a3a5f3e20419ef95ed551581ad597dee75
Syr7-s <isasayar7@github.com>
2021-11-15 22:59:37 +0300

    InstructorLecDto was modified.

--- instructor-service/src/main/java/com/syrisa/instructorservice/dto/InstructorLecDto.java
+++ instructor-service/src/main/java/com/syrisa/instructorservice/dto/InstructorLecDto.java
@@ -1,4 +1,19 @@
 package com.syrisa.instructorservice.dto;
 
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+
+import java.time.LocalDate;
+
+@Data
+@Builder
 public class InstructorLecDto {
+    @Id
+    private Long instructorLecID;
+    private String lectureCode;
+    private String instructorID;
+    private String instructorNameSurname;
+    private int studentCapacity;
+    private LocalDate openDate;
 }
