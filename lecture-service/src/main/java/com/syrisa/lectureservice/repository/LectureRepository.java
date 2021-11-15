f3842cfc8de1a5f8d2a7aba8fce6e54fe6846b7a
Syr7-s <isasayar7@github.com>
2021-11-15 23:35:10 +0300

    findLectureByLectureCode add to lecture-service.

--- lecture-service/src/main/java/com/syrisa/lectureservice/repository/LectureRepository.java
+++ lecture-service/src/main/java/com/syrisa/lectureservice/repository/LectureRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface LectureRepository extends MongoRepository<Lecture,Integer> {
+    Lecture findLectureByLectureCode(String lectureCode);
 }
