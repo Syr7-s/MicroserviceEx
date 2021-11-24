e90a65d0bdc7513a1b5a84bb5098e76466ab118e
İsa SAYAR <isa.sayar1725@gmail.com>
2021-11-24 20:21:39 +0300

    InstructorController was modified.

--- instructor-service/src/main/java/com/syrisa/instructorservice/controller/InstructorController.java
+++ instructor-service/src/main/java/com/syrisa/instructorservice/controller/InstructorController.java
@@ -1,13 +1,16 @@
 package com.syrisa.instructorservice.controller;
 
 import com.syrisa.instructorservice.dto.InstructorDto;
+import com.syrisa.instructorservice.entity.impl.Instructor;
 import com.syrisa.instructorservice.service.InstructorService;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
+
+import javax.validation.constraints.Min;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/v1/")
@@ -19,11 +22,46 @@ public class InstructorController {
     }
 
     @PostMapping("/instructor")
-    public InstructorDto create(@RequestBody InstructorDto instructorDto){
-        try{
+    public InstructorDto create(@RequestBody InstructorDto instructorDto) {
+        try {
             return instructorService.create(instructorDto.toInstructor()).toInstructorDto();
+        } catch (Exception exception) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, exception.getMessage());
+        }
+    }
+
+    @PutMapping("/edit")
+    public InstructorDto update(@RequestBody InstructorDto instructorDto) {
+        try {
+            return instructorService.update(instructorDto.toInstructor()).toInstructorDto();
+        } catch (Exception exception) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, exception.getMessage());
+        }
+    }
+
+    @GetMapping("/{id}")
+    public InstructorDto getById(@PathVariable("id") Long id) {
+        try {
+            return instructorService.getByID(id).toInstructorDto();
+        } catch (Exception exception) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, exception.getMessage());
+        }
+    }
+
+    @GetMapping(value = "/lectures", params = {"page", "size"})
+    public List<InstructorDto> getAll(@Min(0) int page, @Min(1) int size) {
+        return instructorService.getAll(PageRequest.of(page, size))
+                .stream()
+                .map(Instructor::toInstructorDto)
+                .collect(Collectors.toList());
+    }
+
+    @DeleteMapping("/undo/{id}")
+    public String delete(@PathVariable("id") Long id){
+        try {
+            return instructorService.delete(id);
         }catch (Exception exception){
-            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR,exception.getMessage());
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, exception.getMessage());
         }
     }
 }
