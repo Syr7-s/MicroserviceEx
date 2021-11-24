0dc76e76b0dcd0f4adb875ca4d4b56d301b42587
İsa SAYAR <isa.sayar1725@gmail.com>
2021-11-24 20:56:21 +0300

    Entity interface and StudentLecture were created.

---
+++ student-service/src/main/java/com/syrisa/studentservice/entity/Entity.java
@@ -0,0 +1,4 @@
+package com.syrisa.studentservice.entity;
+
+public interface Entity {
+}
---
+++ student-service/src/main/java/com/syrisa/studentservice/entity/StudentLecture.java
@@ -0,0 +1,23 @@
+package com.syrisa.studentservice.entity;
+
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDate;
+
+@Data
+@Builder
+@Document
+public class StudentLecture implements Entity{
+    @Id
+    private Long studentLecID;
+    private LocalDate lastUpdated;
+    private int status;
+    private String studentID;
+    private String studentNameAndSurname;
+    private String lectureCode;
+    private String lectureName;
+
+}
