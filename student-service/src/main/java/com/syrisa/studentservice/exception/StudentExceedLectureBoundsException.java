d52837cfa4a1c3e2c9572886cf383e5abfc8067e
İsa SAYAR <isa.sayar1725@gmail.com>
2021-11-25 20:34:29 +0300

    StudentExceedLectureBoundsException class was created.

---
+++ student-service/src/main/java/com/syrisa/studentservice/exception/StudentExceedLectureBoundsException.java
@@ -0,0 +1,7 @@
+package com.syrisa.studentservice.exception;
+
+public class StudentExceedLectureBoundsException extends Exception{
+    public StudentExceedLectureBoundsException(String message) {
+        super(message);
+    }
+}
