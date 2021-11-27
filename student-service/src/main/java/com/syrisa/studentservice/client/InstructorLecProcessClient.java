aecdaf21d0acee0dbd29328e9443b81e1e3d8fd7
İsa SAYAR <isa.sayar1725@gmail.com>
2021-11-27 16:22:36 +0300

    InstructorLecProcessClient was modified.

--- student-service/src/main/java/com/syrisa/studentservice/client/InstructorLecProcessClient.java
+++ student-service/src/main/java/com/syrisa/studentservice/client/InstructorLecProcessClient.java
@@ -5,10 +5,15 @@ import com.syrisa.studentservice.entity.impl.Entity;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @FeignClient("instructor-service")
 public interface InstructorLecProcessClient {
     @GetMapping("/api/v1//lectureCode/{lectureCode}")
     InstructorLecDto getById(@PathVariable("lectureCode") String lectureCode);
+
+    @PutMapping("/edit")
+    InstructorLecDto update(@RequestBody InstructorLecDto instructorLecDto);
 
 }
