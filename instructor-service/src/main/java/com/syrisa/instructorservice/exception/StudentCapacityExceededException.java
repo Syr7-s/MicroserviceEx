7a3a163c4e62e92ce98aa69c578531cdb9042cd7
İsa SAYAR <isa.sayar1725@gmail.com>
2021-12-03 20:39:32 +0300

    Some files were deleted.

--- instructor-service/src/main/java/com/syrisa/instructorservice/exception/StudentCapacityExceededException.java
+++
@@ -1,7 +0,0 @@
-package com.syrisa.instructorservice.exception;
-
-public class StudentCapacityExceededException extends Exception{
-    public StudentCapacityExceededException(String message) {
-        super(message);
-    }
-}
--- instructor-service/src/main/java/com/syrisa/instructorservice/utility/generate/Generate.java
+++
@@ -1,5 +0,0 @@
-package com.syrisa.instructorservice.utility.generate;
-
-public interface Generate <T>{
-    T generate(int numLength);
-}
