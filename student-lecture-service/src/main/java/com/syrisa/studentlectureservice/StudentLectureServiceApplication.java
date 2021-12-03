57c66283ade3e4ca59234fe34a5431e3406d3eb5
İsa SAYAR <isa.sayar1725@gmail.com>
2021-12-03 20:45:56 +0300

    inial commit and necessary annotations were added.

---
+++ student-lecture-service/src/main/java/com/syrisa/studentlectureservice/StudentLectureServiceApplication.java
@@ -0,0 +1,13 @@
+package com.syrisa.studentlectureservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentLectureServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentLectureServiceApplication.class, args);
+    }
+
+}
---
+++ student-lecture-service/src/test/java/com/syrisa/studentlectureservice/StudentLectureServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.syrisa.studentlectureservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentLectureServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
