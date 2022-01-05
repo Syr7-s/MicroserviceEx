201b2d9e1cdef9d72bf4d613209a6ec8eae6afbd
İsa SAYAR <isa.sayar1725@gmail.com>
2022-01-05 21:22:26 +0300

    some classes were modified.

--- web-app-bff/src/main/java/com/syrisa/webappbff/service/impl/StudentLectureServiceImpl.java
+++ web-app-bff/src/main/java/com/syrisa/webappbff/service/impl/StudentLectureServiceImpl.java
@@ -1,8 +1,43 @@
 package com.syrisa.webappbff.service.impl;
 
+import com.syrisa.webappbff.client.StudentLectureServiceClient;
+import com.syrisa.webappbff.entity.Student;
+import com.syrisa.webappbff.entity.StudentLecture;
+import com.syrisa.webappbff.service.StudentLectureService;
+import com.syrisa.webappbff.service.StudentService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
-public class StudentLectureServiceImpl {
+@RequiredArgsConstructor
+public class StudentLectureServiceImpl implements StudentLectureService {
+    private final StudentLectureServiceClient studentLectureServiceClient;
+    private final StudentService studentService;
 
+    @Override
+    public StudentLecture create(StudentLecture studentLecture) {
+        Student student = studentService.findByStudentNumber(studentLecture.getStudentNumber());
+
+        return null;
+    }
+
+    @Override
+    public StudentLecture update(StudentLecture studentLecture) {
+        return null;
+    }
+
+    @Override
+    public String delete(Long id) {
+        return null;
+    }
+
+    @Override
+    public StudentLecture getById(Long id) {
+        return null;
+    }
+
+    @Override
+    public StudentLecture findStudentLectureByStudentNumber(Long studentNumber) {
+        return null;
+    }
 }
