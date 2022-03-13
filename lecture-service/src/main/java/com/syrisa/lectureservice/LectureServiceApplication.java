9b1a46aadfe9130077da1eab288bfc107dcd05ec
İsa SAYAR <isa.sayar1725@gmail.com>
2022-03-13 22:27:01 +0300

    files were edited.

--- lecture-service/src/main/java/com/syrisa/lectureservice/LectureServiceApplication.java
+++ lecture-service/src/main/java/com/syrisa/lectureservice/LectureServiceApplication.java
@@ -2,12 +2,13 @@ package com.syrisa.lectureservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableEurekaClient
 @EnableFeignClients
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class LectureServiceApplication {
 
     public static void main(String[] args) {
